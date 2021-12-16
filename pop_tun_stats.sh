9ca4d0b0d45362a872c2c1d8a08be89078b2c64c
Varun Tewari <vtewari@netskope.com>
2021-12-15 21:57:37 -0800

    Add Tun stats

--- pop_tun_stats.sh
+++ pop_tun_stats.sh
@@ -5,4 +5,14 @@ for tunc in "${tunc_array[@]}"
 do
     echo "Tx Dropped: /sys/class/net/$tunc/statistics/tx_dropped"
     cat /sys/class/net/$tunc/statistics/tx_dropped
+
+    echo "Tx Error: /sys/class/net/$tunc/statistics/tx_errors"
+    cat /sys/class/net/$tunc/statistics/tx_errors
+
+
+    echo "Rx Dropped: /sys/class/net/$tunc/statistics/rx_dropped"
+    cat /sys/class/net/$tunc/statistics/rx_dropped
+
+    echo "Rx Error: /sys/class/net/$tunc/statistics/rx_error"
+    cat /sys/class/net/$tunc/statistics/rx_errors
 done
