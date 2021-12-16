d5b97a4166cdb5a3efff006a6a5d8ddbed1cc0d4
Varun Tewari <vtewari@netskope.com>
2021-12-15 23:28:36 -0800

    Tunnel count

--- pop_tunnel_count.sh
+++ pop_tunnel_count.sh
@@ -4,5 +4,13 @@ pop_array=($(cat sft_servers.list| grep ipsec | awk '{print $1}'))
 for pop in "${pop_array[@]}"
 do
     echo "sft ssh $pop --command 'date && sudo swanctl --list-sas | grep ESTAB | wc -l'"
+    echo "Ike SAs"
     sft ssh $pop --command 'date && sudo swanctl --list-sa | grep ESTAB | wc -l'
+
+    echo "IPsec SAs"
+    sft ssh $pop --command 'date && sudo swanctl --list-sa | grep TUNNEL-in-UDP | wc -l'
+
+    echo "Tunc Count"
+    sft ssh $pop --command 'date && ip link | grep tunc | wc -l'
+
 done
