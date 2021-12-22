1abe1be5239fe14c8feb1655dcabaf4ff4ee983f
Varun Tewari <vtewari@netskope.com>
2021-12-21 16:05:50 -0800

    Updating pop tun stats

--- pop_tun_stats.sh
+++ pop_tun_stats.sh
@@ -1,19 +1,24 @@
-#!/bin/zsh
+#!/bin/bash
 
 tunc_array=($(ifconfig | grep tun | awk '{print $1}'))
-date
-for tunc in "${tunc_array[@]}"
+for i in {1 . . 4}
 do
-    echo "Tx Dropped: /sys/class/net/$tunc/statistics/tx_dropped"
-    cat /sys/class/net/$tunc/statistics/tx_dropped
+    date
+    for tunc in "${tunc_array[@]}"
+    do
+        echo "Tx Dropped: /sys/class/net/$tunc/statistics/tx_dropped"
+	cat /sys/class/net/$tunc/statistics/tx_dropped
 
-    echo "Tx Error: /sys/class/net/$tunc/statistics/tx_errors"
-    cat /sys/class/net/$tunc/statistics/tx_errors
+	echo "Tx Error: /sys/class/net/$tunc/statistics/tx_errors"
+	cat /sys/class/net/$tunc/statistics/tx_errors
 
 
-    echo "Rx Dropped: /sys/class/net/$tunc/statistics/rx_dropped"
-    cat /sys/class/net/$tunc/statistics/rx_dropped
+	echo "Rx Dropped: /sys/class/net/$tunc/statistics/rx_dropped"
+	cat /sys/class/net/$tunc/statistics/rx_dropped
 
-    echo "Rx Error: /sys/class/net/$tunc/statistics/rx_error"
-    cat /sys/class/net/$tunc/statistics/rx_errors
+	echo "Rx Error: /sys/class/net/$tunc/statistics/rx_error"
+	cat /sys/class/net/$tunc/statistics/rx_errors
+    done
+echo "Waiting for 10 seconds"
+sleep 10
 done
