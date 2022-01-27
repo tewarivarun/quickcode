66f8b62abcfe6e23948d23bb96f1e052168876c5
Varun Tewari <vtewari@netskope.com>
2022-01-27 14:27:29 -0800

    Adding scripts

---
+++ create_ipsec_ns_pop.sh
@@ -0,0 +1,48 @@
+#!/bin/zsh
+
+for (( i=10; i<=245; i++ ))
+do
+echo "conn tenant-1-10$i"
+echo "    rightid=tenant.1.10$i"
+echo ""
+done
+
+# 200 IPSec Connections
+for (( i=10; i<=210; i++ ))
+do
+echo "conn tenant-1-20$i"
+echo "    rightid=tenant.1.20$i"
+echo ""
+done
+
+# 200 IPSec Connections
+for (( i=10; i<=210; i++ ))
+do
+echo "conn tenant-1-30$i"
+echo "    rightid=tenant.1.30$i"
+echo ""
+done
+
+# 200 IPSec Connections
+for (( i=10; i<=210; i++ ))
+do
+echo "conn tenant-1-40$i"
+echo "    rightid=tenant.1.40$i"
+echo ""
+done
+
+# 200 IPSec Connections
+for (( i=10; i<=210; i++ ))
+do
+echo "conn tenant-1-50$i"
+echo "    rightid=tenant.1.50$i"
+echo ""
+done
+
+# 200 IPSec Connections
+for (( i=10; i<=210; i++ ))
+do
+echo "conn tenant-1-60$i"
+echo "    rightid=tenant.1.60$i"
+echo ""
+done
