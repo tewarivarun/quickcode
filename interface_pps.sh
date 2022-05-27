3109b47fb80c606871b46bf5608931c43eadbf60
Varun Tewari <vtewari@netskope.com>
2022-05-27 07:50:24 -0700

    Updating pps script

--- interface_pps.sh
+++ interface_pps.sh
@@ -18,10 +18,16 @@ while true
 do
         R1=`cat /sys/class/net/$1/statistics/rx_packets`
         T1=`cat /sys/class/net/$1/statistics/tx_packets`
+        DRX1=`cat /sys/class/net/$1/statistics/rx_dropped`
+        DTX1=`cat /sys/class/net/$1/statistics/tx_dropped`
         sleep $INTERVAL
         R2=`cat /sys/class/net/$1/statistics/rx_packets`
         T2=`cat /sys/class/net/$1/statistics/tx_packets`
+        DRX2=`cat /sys/class/net/$1/statistics/rx_dropped`
+        DTX2=`cat /sys/class/net/$1/statistics/tx_dropped`
         TXPPS=`expr $T2 - $T1`
         RXPPS=`expr $R2 - $R1`
-        echo "TX $1: $TXPPS pkts/s RX $1: $RXPPS pkts/s"
+        DRXPPS=`expr $DRX2 - $DRX1`
+        DTXPPS=`expr $DTX2 - $DTX1`
+        echo "$1: TX : $TXPPS pkts/s RX : $RXPPS pkts/s DRX : $DRXPPS pkts/s DTX : $DTXPPS pkts/s"
 done
